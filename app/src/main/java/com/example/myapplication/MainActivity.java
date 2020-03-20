ca69504530088e3de9adb17805ea1e64e54d3e9d
Priyanka11mishra <priyanka1109mishra@gmail.com>
2020-03-20 16:36:12 +0530

    CountryCodePicker

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.myapplication;
 
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends Activity {
+    BottomNavigationView navigationView;
     CountryCodePicker ccp;
     Button btncpp;
 
@@ -19,6 +26,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         ccp=(CountryCodePicker)findViewById(R.id.cpp);
         btncpp=(Button)findViewById(R.id.cpbtn);
+        navigationView = findViewById(R.id.botomnav);
 
         btncpp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +35,35 @@ public class MainActivity extends Activity {
                 startActivity(intent);
             }
         });
+        openFragment(HomeFragment.newInstance("",""));
+
+
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.home1:
+                    openFragment(HomeFragment.newInstance("", ""));
+                    return true;
+                case R.id.search:
+                    openFragment(SearchFragment.newInstance("", ""));
+                    return true;
+                case R.id.attachment:
+                    openFragment(AttachmentFragment.newInstance("", ""));
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
 }
